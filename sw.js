5eac41be3a03839ca6130604c4b5b0f16b6d61f0
cworkfox-source <cworkfox@gmail.com>
2026-09-13 17:07:12 +0800

    新增重複資料掃描與日期分析

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kid-running-v6';
+const CACHE='kid-running-v7';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./core.js','./db.js','./auth.js','./backup.js','./restore.js','./firebase.js','./firebase-config.js','./cloud.js'];
 
 self.addEventListener('install',event=>{
